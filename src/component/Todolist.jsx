ea1036f77fec230e801498089a5a7ef5d9bec921
mida2709 <126259059+mida2709@users.noreply.github.com>
2023-05-09 09:41:50 +0700

    Add files via upload

---
+++ src/component/Todolist.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+  state = {
+    tasks: [
+      { id: 1, name: 'Mengerjakan Post Test', completed: false },
+      { id: 2, name: 'Mengerjakan Pre Test', completed: true },
+      { id: 3, name: 'Menghadiri Bootcamp PZ', completed: false },
+    ],
+  };
+
+  handleTaskClick = (id) => {
+    const updatedTasks = this.state.tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, completed: !task.completed };
+      } else {
+        return task;
+      }
+    });
+
+    this.setState({ tasks: updatedTasks });
+  };
+
+  render() {
+    const tasks = this.state.tasks.map((task) => (
+      <li
+        key={task.id}
+        onClick={() => this.handleTaskClick(task.id)}
+        style={{ textDecoration: task.completed ? 'line-through' : 'none' }}
+      >
+        {task.name}
+      </li>
+    ));
+
+    return (
+      <div>
+        <h1>Todo List</h1>
+        <ul>{tasks}</ul>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
